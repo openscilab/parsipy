218579cfc2e4d9f379997f07169cab16076b2e40
Sepand Haghighi <sepand.haghighi@yahoo.com>
2025-01-28 01:31:47 +0330

    Data (#6)
    * fix : some data moved to data.py
    
    * fix : p2g.py data moved to data.py
    
    * fix : roots moved to data.py
    
    * fix : stems moved to data.py
    
    * fix : data files moved to data folder
    
    * feat : MANIFEST.in added

--- parsipy/word_stemmer.py
+++ parsipy/word_stemmer.py
@@ -1,104 +1,12 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import copy
-
-df_ = list(pd.read_json('roots.json')['data'])
-
-prefixes_dic = {
-    'a': '',
-    'an': '',
-    'abē': 'ʾp̄y',
-    'duš': 'dwš',
-    'ham': 'hm',
-    'hām': 'hʾm',
-    'hu': 'hw',
-    'jud': "ywdt'"
-}
-postfixes_dict = {
-    'am': 'm',
-    'ēd': "yt'",
-    'ēnd': 'd',
-    'ag': "k'",
-    'āg': "'k",
-    'agān': "kʾn",
-    'an': 'n',
-    'ān': "ʾn",
-    'ānag': "ʾnk",
-    'ār': "ʾl",
-    'āwand': "ʾwnd",
-    'bān': "bʾn",
-    'bad': 'pt',
-    'bed': 'pt',
-    'dān': "dʾn",
-    'ēn': "yn",
-    'endag': "yndk",
-    'estān': "stʾn",
-    'gāh': "gʾs",
-    'gānag': "kʾnk",
-    'gar': "kl",
-    'gār': "kʾl",
-    'gēn': "kyn",
-    'īg': "yk",
-    'īgān': "ykʾn",
-    'īh': "yh",
-    'īha': "yhʾ",
-    'īhā': "yh'",
-    'išn': "šn'",
-    'išt': "ʾšt'",
-    'īzag': "yck",
-    'om': "wm",
-    'mand': "mnd",
-    'ōmand': "'wmnd",
-    'rōn': "lwn",
-    'tar': "tl",
-    'dar': "tl",
-    'dār': "tʾl",
-    'tom': "twm",
-    'dom': "twm",
-    'war': "wl",
-    'wār': "wʾl",
-    'zār': "cʾl",
-    'īd': "yt",
-}
-
-transliteration_to_transcription_rules = {
-    "t": ("t"),
-    "š": ("š"),
-    "č": ("c"),
-    "p": ("p"),
-    "f": ("f"),
-    "s": ("s"),
-    "h": ("s"),
-    "l": ("l"),
-    "k": ("k"),
-    "z": ("z"),
-    "r": ("r"),
-    "n": ("n"),
-    "w": ("w", "wb"),
-    "m": ("m", "nb"),
-    "y": ("y"),
-    "g": ("g", "k"),
-    "d": ("d"),
-    "b": ("b", "p"),
-    "a": ("h"),
-    "ā": ("h"),
-    "x": ("h"),
-    "j": ("y"),
-    "ē": ("i"),
-    "e": ("i"),
-    "i": ("i"),
-    "c": ("c"),
-    "u": ('w'),
-    "ī": ('y'),
-    "ō": ('w'),
-    "ū": ('w'),
-    "ǰ": ('c')
-}
-
+from .data import PREFIXES, POSTFIXES
+from .data import ROOTS
 
 def find_root(word: str):
     def is_goal(stem):
-        if stem in df_:
+        if stem in ROOTS:
             return True
         return False
 
@@ -113,7 +21,7 @@ def find_root(word: str):
         if candidate not in visited:
             visited.append(candidate)
 
-        for postfix in postfixes_dict.keys():
+        for postfix in POSTFIXES.keys():
             if candidate['stem'].endswith(postfix):
                 postfixes_list = candidate['post_fixes_list'].copy()
                 prefix_list = candidate['pre_fixes_list'].copy()
@@ -124,7 +32,7 @@ def find_root(word: str):
                 if new_candidate not in visited:
                     candidates.append(new_candidate)
 
-        for prefix in prefixes_dic.keys():
+        for prefix in PREFIXES.keys():
             if candidate['stem'].startswith(prefix):
                 postfixes_list = candidate['post_fixes_list'].copy()
                 prefix_list = candidate['pre_fixes_list'].copy()
