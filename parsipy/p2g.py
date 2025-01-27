135a9b2e10024119d7f7242d8abe85b201d66244
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-01-27 18:49:05 +0330

    fix : p2g.py data moved to data.py

--- parsipy/p2g.py
+++ parsipy/p2g.py
@@ -1,96 +1,9 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import word_stemmer
+from .data import PREFIXES, POSTFIXES
+from .data import TRANSLITERATION_TO_TRANSCRIPTION_RULES
 
-prefixes_dic = {
-    'a': '',
-    'an': '',
-    'abē': 'ʾp̄y',
-    'duš': 'dwš',
-    'ham': 'hm',
-    'hām': 'hʾm',
-    'hu': 'hw',
-    'jud': "ywdt'"
-}
-postfixes_dict = {
-    'am': 'm',
-    'ēd': "yt'",
-    'ēnd': 'd',
-    'ag': "k'",
-    'āg': "'k",
-    'agān': "kʾn",
-    'an': 'n',
-    'ān': "ʾn",
-    'ānag': "ʾnk",
-    'ār': "ʾl",
-    'āwand': "ʾwnd",
-    'bān': "bʾn",
-    'bad': 'pt',
-    'bed': 'pt',
-    'dān': "dʾn",
-    'ēn': "yn",
-    'endag': "yndk",
-    'estān': "stʾn",
-    'gāh': "gʾs",
-    'gānag': "kʾnk",
-    'gar': "kl",
-    'gār': "kʾl",
-    'gēn': "kyn",
-    'īg': "yk",
-    'īgān': "ykʾn",
-    'īh': "yh",
-    'īha': "yhʾ",
-    'īhā': "yh'",
-    'išn': "šn'",
-    'išt': "ʾšt'",
-    'īzag': "yck",
-    'om': "wm",
-    'mand': "mnd",
-    'ōmand': "'wmnd",
-    'rōn': "lwn",
-    'tar': "tl",
-    'dar': "tl",
-    'dār': "tʾl",
-    'tom': "twm",
-    'dom': "twm",
-    'war': "wl",
-    'wār': "wʾl",
-    'zār': "cʾl",
-    'īd': "yt",
-}
-transliteration_to_transcription_rules = {
-    "t": ("t"),
-    "š": ("š"),
-    "č": ("c"),
-    "p": ("p"),
-    "f": ("f"),
-    "s": ("s"),
-    "h": ("s"),
-    "l": ("l"),
-    "k": ("k"),
-    "z": ("z"),
-    "r": ("r"),
-    "n": ("n"),
-    "w": ("w", "wb"),
-    "m": ("m", "nb"),
-    "y": ("y"),
-    "g": ("g", "k"),
-    "d": ("d"),
-    "b": ("b", "p"),
-    "a": ("h"),
-    "ā": ("h"),
-    "x": ("h"),
-    "j": ("y"),
-    "ē": ("i"),
-    "e": ("i"),
-    "i": ("i"),
-    "c": ("c"),
-    "u": ('w'),
-    "ī": ('y'),
-    "ō": ('w'),
-    "ū": ('w'),
-    "ǰ": ('c')
-}
 
 stems = pd.read_csv('stems_new.csv')
 
@@ -100,20 +13,20 @@ def oov_translit(word):
     new_word = ''
     for char in word:
         if first_letter:
-            new_word += transliteration_to_transcription_rules[char][0]
+            new_word += TRANSLITERATION_TO_TRANSCRIPTION_RULES[char][0]
         else:
-            new_word += transliteration_to_transcription_rules[char][-1]
+            new_word += TRANSLITERATION_TO_TRANSCRIPTION_RULES[char][-1]
     return new_word
 
 
 def get_transliteration(stem, post_fixes_list: list, pre_fixes_list: list):
     if post_fixes_list:
-        post_fixes = ''.join(postfixes_dict[post_fix] for post_fix in reversed(post_fixes_list))
+        post_fixes = ''.join(POSTFIXES[post_fix] for post_fix in reversed(post_fixes_list))
     else:
         post_fixes = ''
 
     if pre_fixes_list:
-        pre_fixes = ''.join(prefixes_dic[pre_fix] for pre_fix in reversed(pre_fixes_list))
+        pre_fixes = ''.join(PREFIXES[pre_fix] for pre_fix in reversed(pre_fixes_list))
     else:
         pre_fixes = ''
 
