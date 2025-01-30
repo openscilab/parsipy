ffe24759e45b63d4223462eed7ddd83ff17a25a0
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-01-30 18:30:17 +0330

    fix : minor edits in tokenizer.py

--- parsipy/tokenizer.py
+++ parsipy/tokenizer.py
@@ -1,22 +1,8 @@
 # -*- coding: utf-8 -*-
-def cleaning(text: str):
+def cleaning(text):
     return text.replace('*', '').replace('(w)', '').replace('[', '').replace(']', '').replace('<', '').replace('>',
                                                                                                                '')
 
-
-def preprocess(sentence: str):
+def preprocess(sentence):
     sentence = sentence.replace('-', ' ')
     return cleaning(sentence)
-
-
-def run(sentence: str):
-    sentence = preprocess(sentence)
-    my_list = []
-    for id, word in enumerate(sentence.split()):
-        data = {'id': id, 'text': word}
-        my_list.append(data)
-    return my_list
-
-
-if __name__ == '__main__':
-    print(run('ruwān ī xwēš andar ayād dār nām ī xwēš rāy xwēš-kārīh '))
