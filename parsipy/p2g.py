744e56d8cf265042b9ca910ac4795f7ceadfb2b6
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-02-08 14:53:51 -0800

    update : p2g module updated.

--- parsipy/p2g.py
+++ parsipy/p2g.py
@@ -5,7 +5,15 @@ from .data import TRANSLITERATION_TO_TRANSCRIPTION_RULES
 from .data import STEMS
 
 
-def oov_translit(word):
+def oov_transliterate(word):
+    """
+    Out of vocabulary transliteration function.
+    This function transliterates a word that is not in the STEMS dictionary.
+    
+    :param word: word to be transliterated
+    :type word: str
+    :return: str
+    """
     first_letter = True
     new_word = ''
     for char in word:
@@ -17,6 +25,17 @@ def oov_translit(word):
 
 
 def get_transliteration(stem, post_fixes_list, pre_fixes_list):
+    """
+    This function returns the transliteration of a word given its stem and affixes.
+
+    :param stem: word stem
+    :type stem: str
+    :param post_fixes_list: list of post-fixes
+    :type post_fixes_list: list
+    :param pre_fixes_list: list of pre-fixes
+    :type pre_fixes_list: list
+    :return: str
+    """
     if post_fixes_list:
         post_fixes = ''.join(POSTFIXES[post_fix] for post_fix in reversed(post_fixes_list))
     else:
@@ -31,13 +50,22 @@ def get_transliteration(stem, post_fixes_list, pre_fixes_list):
         if stem_ == stem:
             return str(pre_fixes) + str(transit_) + str(post_fixes)
     # Return None if no match is found to ensure consistent function behavior
-    return oov_translit(stem)
+    return oov_transliterate(stem)
 
 
 def run(sentence):
+    """
+    This function takes a sentence as input and returns a list of dictionaries containing the word, its transliteration,
+    and its transcription.
+
+    :param sentence: sentence to be processed
+    :type sentence: str
+    :return: list
+    """
     result = []
     for word in sentence.split():
-        roots = word_stemmer.find_root(word)
-        data = {'text': word, 'transliteration': get_transliteration(**roots)}
+        roots = find_root(word)
+        data = {'text': word,
+                'transliteration': get_transliteration(**roots)}
         result.append(data)
     return result
