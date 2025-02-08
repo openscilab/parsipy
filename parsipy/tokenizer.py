3110ff279ab2bcdbd47808698d58a5166fa36dbb
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-02-08 15:06:46 -0800

    update : tokenizer updated.

--- parsipy/tokenizer.py
+++ parsipy/tokenizer.py
@@ -1,17 +1,44 @@
 # -*- coding: utf-8 -*-
 def cleaning(text):
-    return text.replace('*', '').replace('(w)', '').replace('[', '').replace(']', '').replace('<', '').replace('>',
-                                                                                                               '')
+    """
+    Cleaning the text from special characters
+    
+    :param text: input text
+    :type text: str
+    :return: cleaned text
+    """
+    return text.replace('*', '')\
+                .replace('(w)', '')\
+                .replace('[', '')\
+                .replace(']', '')\
+                .replace('<', '')\
+                .replace('>', '')
+
 
 def preprocess(sentence):
+    """
+    Preprocess the sentence.
+
+    :param sentence: input sentence
+    :type sentence: str
+    :return: preprocessed sentence
+    """
     sentence = sentence.replace('-', ' ')
     return cleaning(sentence)
 
 
 def run(sentence):
+    """
+    Tokenize the sentence.
+
+    :param sentence: input sentence
+    :type sentence: str
+    :return: list of tokenized words
+    """
     sentence = preprocess(sentence)
     result = []
     for index, word in enumerate(sentence.split()):
-        data = {'id': index, 'text': word}
+        data = {'id': index,
+                'text': word}
         result.append(data)
     return result
