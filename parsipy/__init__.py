061258309f1a92d60a2361a96f257c3e2067b462
sepandhaghighi <sepand.haghighi@yahoo.com>
2025-02-08 21:01:02 +0330

    fix : __init__ file updated

--- parsipy/__init__.py
+++ parsipy/__init__.py
@@ -2,7 +2,7 @@
 from .params import PARSIPY_VERSION
 from .word_stemmer import find_root
 from .tokenizer import preprocess, cleaning
-from .pos_tagger import POSLoader, POSTagger
+from .pos_tagger import POSTaggerRuleBased
 from .p2g import oov_translit, get_transliteration
 
 __version__ = PARSIPY_VERSION
